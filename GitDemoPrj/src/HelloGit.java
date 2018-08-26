d23a93c5193b228e234050ae1773faa8f64260d7
kkucheria <kanika.sogani@gmail.com>
2018-08-26 09:57:40 +0530

    1.1

---
+++ GitDemoPrj/src/HelloGit.java
@@ -0,0 +1,11 @@
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hii Git..how are you");
+		System.out.println("********");
+
+	}
+
+}
